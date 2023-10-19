85f06fdca3fbeddbe70a399c803abf7ad290c764
Ju / smwhr <ju@smwhr.net>
2023-10-19 17:12:52 +0200

    compile on playdate

--- source/libs/lume.lua
+++ source/libs/lume.lua
@@ -706,42 +706,6 @@ function lume.uuid()
 end
 
 
-function lume.hotswap(modname)
-  local oldglobal = lume.clone(_G)
-  local updated = {}
-  local function update(old, new)
-    if updated[old] then return end
-    updated[old] = true
-    local oldmt, newmt = getmetatable(old), getmetatable(new)
-    if oldmt and newmt then update(oldmt, newmt) end
-    for k, v in pairs(new) do
-      if type(v) == "table" then update(old[k], v) else old[k] = v end
-    end
-  end
-  local err = nil
-  local function onerror(e)
-    for k in pairs(_G) do _G[k] = oldglobal[k] end
-    err = lume.trim(e)
-  end
-  local ok, oldmod = pcall(require, modname)
-  oldmod = ok and oldmod or nil
-  xpcall(function()
-    package.loaded[modname] = nil
-    local newmod = import(modname)
-    if type(oldmod) == "table" then update(oldmod, newmod) end
-    for k, v in pairs(oldglobal) do
-      if v ~= _G[k] and type(v) == "table" then
-        update(v, _G[k])
-        _G[k] = v
-      end
-    end
-  end, onerror)
-  package.loaded[modname] = oldmod
-  if err then return nil, err end
-  return oldmod
-end
-
-
 local ripairs_iter = function(t, i)
   i = i - 1
   local v = t[i]
